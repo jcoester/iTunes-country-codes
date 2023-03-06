adae888abe5c758b81c4a708612c115dc791f020
jcoester <12155189+jcoester@users.noreply.github.com>
2023-03-06 16:58:32 +0100

    Add files

---
+++ script/combine_chunks.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import json
+import os
+import pandas as pd
+
+path = "./chunks/"
+file_list = os.listdir(path)
+full = ""
+
+# 1. Combine chunk data
+for file in file_list:
+    with open(path + file, "r") as f:
+        data = f.read()
+        # valid codes found in chunk
+        if data.rstrip() != "":
+            full = full + data.rstrip() + ","
+
+itunes_codes = full.split(",")
+itunes_codes = list(filter(None, itunes_codes))
+
+# 2. Load list of alpha-2 country names
+df_countries = pd.read_csv(
+    "script/alpha2_countries.csv", encoding="utf8", keep_default_na=False
+).set_index("alpha2")
+
+dict_countries = df_countries["country"].to_dict()
+
+# 3. Match supported iTunes codes with alpha-2 country names
+itunes_countries = {}
+for alpha2 in itunes_codes:
+    try:
+        itunes_countries[alpha2] = dict_countries[alpha2]
+    except KeyError as e:
+        print(e, "is an unknown Country code. Please update the CSV table.")
+        itunes_countries[alpha2] = "-"
+
+new = dict(sorted(itunes_countries.items(), key=lambda item: item[1]))
+
+# 4. Detect changes
+try:
+    old = json.load(open("itunes_country_codes.json"))
+except FileNotFoundError:
+    old = {}
+
+diffAdd = sorted(new.keys() - old)
+diffRm = sorted(old.keys() - new)
+diffValues = sorted([key for key in old.keys() & new if old[key] != new[key]])
+writeFile = False
+
+# 5. Output changes
+if len(diffAdd) > 0:
+    writeFile = True
+    print("• Added:", diffAdd)
+
+if len(diffRm) > 0:
+    writeFile = True
+    print("• Removed:", diffRm)
+
+if old != new:
+    writeFile = True
+    for diff in sorted(diffValues):
+        print("• Updated country name(s):", diffValues)
+
+if writeFile:
+    with open("itunes_country_codes.json", "w", encoding="utf-8") as f:
+        json.dump(new, f, ensure_ascii=False, indent=4)
+else:
+    print("identical")
---
+++ script/fetch_itunes_country_codes.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import json
+import sys
+import requests
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
+
+
+def fetch_iTunes_countries(list_alpha2):
+    whitelist = []
+
+    # setup session to use retries when API throttles
+    s = requests.Session()
+    retry_strategy = Retry(
+        total=100, backoff_factor=5, status_forcelist=[403, 500, 502, 503, 504]
+    )
+    s.mount("https://", HTTPAdapter(max_retries=retry_strategy))
+
+    # search itunes country store
+    api = "https://itunes.apple.com/search?"
+    for alpha2 in list_alpha2:
+        response = s.get(api + "limit=1&term=app&country=" + alpha2)
+
+        # check for valid response
+        if response.status_code == 200:
+            whitelist.append(alpha2)
+
+    return whitelist
+
+
+def split(alist, n):
+    for i in range(0, n):
+        yield alist[i::n]
+
+
+def main():
+    # 1. Load list of all 676 available ISO 3166-1 alpha-2 codes
+    # From https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2
+    list_alpha2 = json.load(open("script/iso3166_1_alpha2.json"))
+    assert len(list_alpha2) == 676
+
+    # 2. Split into chunks to enable parallel execution
+    chunk_size = int(sys.argv[1])
+    chunk_num = int(sys.argv[2])
+    chunks = list(split(list_alpha2, chunk_size))
+    chunk = chunks[chunk_num]
+
+    # 3. Fetch list of all codes that return 200 OK status
+    whitelist = fetch_iTunes_countries(chunk)
+
+    # 4. Output list as str
+    print(",".join(whitelist))
+
+
+if __name__ == "__main__":
+    main()
